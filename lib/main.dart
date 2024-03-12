e1b5206664a56aa57a882e46ff601df5e1c9c512
Ariirau FUCKS <ariirau.fucks@epitech.eu>
2024-03-12 10:00:56 +0100

    refactor: try other authentification method

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,121 @@
+// import 'package:flutter/material.dart';
+// import 'package:url_launcher/url_launcher.dart';
+
+// final Uri _url = Uri.parse(
+//     'https://trello.com/1/authorize?expiration=never&scope=read,write,account&response_type=token&key=fddd47f32d57f1779100f625dc67f2b6');
+
+// void main() => runApp(
+//       const MaterialApp(
+//         home: Material(
+//           child: Center(
+//             child: ElevatedButton(
+//               onPressed: _launchUrl,
+//               child: Text('Connect with Trello account'),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+
+// Future<void> _launchUrl() async {
+//   if (!await launchUrl(_url)) {
+//     throw Exception('Could not launch $Uri');
+//   }
+//   print(closeInAppWebView());
+// }
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:web_scraper/web_scraper.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  final authUrl = Uri.parse(
+      "https://trello.com/1/authorize?expiration=never&scope=read,write,account&response_type=token&key=fddd47f32d57f1779100f625dc67f2b6");
+  final String redirectUrl = "https://trello.com/1/token/approve";
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Authentification à Trello'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: () async {
+            await openUrlAndScrape(authUrl, redirectUrl);
+          },
+          child: Text('Se connecter avec Trello'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<void> openUrlAndScrape(Uri url, String redirectUrl) async {
+    print("uri = " + url.toString());
+    if (await canLaunch(url.toString())) {
+      await launch(url.toString(), forceSafariVC: false, forceWebView: false);
+      print("ouverture lien");
+      // Wait for the browser to be opened and then scrape data.
+      await Future.delayed(Duration(seconds: 5)); // Adjust as needed.
+
+      // Check if the current URL matches the specified redirect URL.
+      print("get current url");
+      if (await currentUrlMatches(redirectUrl)) {
+        // Scrape data if the URL matches.
+        print("debut scraping");
+        await scrapeDataAndClose(redirectUrl);
+        print("fermeture lien");
+      }
+    } else {
+      throw 'Impossible d\'ouvrir le lien $url';
+    }
+  }
+
+  Future<bool> currentUrlMatches(String expectedUrl) async {
+    final currentUrl = await getCurrentUrl();
+    return currentUrl == expectedUrl;
+  }
+
+  Future<String> getCurrentUrl() async {
+    // You may need to use the appropriate method to retrieve the current URL.
+    // For example, if you're using a web view, you can use the webview_flutter package.
+    // Modify this method based on your specific implementation.
+    return 'https://www.trello.com/1/token/approve'; // Placeholder URL, replace with the actual method.
+  }
+
+  Future<void> scrapeDataAndClose(String redirectUrl) async {
+    final webScraper = WebScraper(redirectUrl);
+
+    // Wait for the page to load.
+    await Future.delayed(Duration(seconds: 3)); // Adjust as needed.
+
+    if (await webScraper.loadWebPage('/')) {
+      // Scrape the data you need.
+      print("scrapped uri : " + webScraper.baseUrl.toString());
+      print("scrapped text : " + webScraper.getPageContent().toString());
+      final preElement = webScraper.getElement('.surface .content', ['pre']);
+      final scrapedText = preElement;
+
+      // Display or use the scraped data.
+      print('Scraped Text: $scrapedText');
+
+      // Close the window.
+      navigatorKey.currentState?.pop();
+    } else {
+      print('Impossible de charger la page pour le scraping.');
+    }
   }
 }
