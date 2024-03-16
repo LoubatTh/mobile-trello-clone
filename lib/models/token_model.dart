070152651c1b4fad71e90e73a1cadaf64ed46d5b
Ariirau FUCKS <ariirau.fucks@epitech.eu>
2024-03-16 13:45:53 +0100

    feat: add auth (bug)

---
+++ lib/models/token_model.dart
@@ -0,0 +1,9 @@
+class Token {
+  final int id;
+  final String token;
+
+  const Token({
+    required this.id,
+    required this.token,
+  });
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,23 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-final Uri _url = Uri.parse('https://flutter.dev');
+final Uri _url = Uri.parse(
+    'https://trello.com/1/authorize?expiration=never&scope=read,write,account&response_type=token&key=fddd47f32d57f1779100f625dc67f2b6');
 
-void main() => runApp(
-      const MaterialApp(
-        home: Material(
-          child: Center(
-            child: ElevatedButton(
-              onPressed: _launchUrl,
-              child: Text('Show Flutter homepage'),
+class MyHomePage extends StatelessWidget {
+  final TextEditingController _textController = TextEditingController();
+
+  MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => _launchInWebView(),
+              child: const Text('Extract your Trello token'),
             ),
-          ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your token here',
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () => _sendToken(_textController.text),
+                  child: const Text('Send Token'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
+  }
 
-Future<void> _launchUrl() async {
-  if (!await launchUrl(_url)) {
+  void _sendToken(String token) {
+    // Utilisez la valeur du token ici
+    print('Token: $token');
+    // Vous pouvez implémenter ici la logique pour envoyer le token.
+  }
+}
+
+Future<void> _launchInWebView() async {
+  if (!await launchUrl(_url, mode: LaunchMode.inAppBrowserView)) {
     throw Exception('Could not launch $_url');
   }
 }
