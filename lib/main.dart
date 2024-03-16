d9837813a45a0c13b5ca3d5166e88fe6486083d7
Ariirau FUCKS <ariirau.fucks@epitech.eu>
2024-03-16 16:15:09 +0100

    feat: add auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: MyHomePage(),
-      ),
-    );
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    MaterialApp(
+      home: MyHomePage(),
+    ),
+  );
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,4 @@
-import 'package:app/services/database_helper.dart';
+import 'package:app/services/user_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -47,18 +47,9 @@ class MyHomePage extends StatelessWidget {
 
   void _sendToken(String token) async {
     final dbHelper = DatabaseHelper.instance;
-    final existingToken = await dbHelper.getFirstToken();
-    if (existingToken == null) {
-      // Créez la base de données si elle n'existe pas
-      await dbHelper.insertToken(token);
-    } else {
-      // Remplacez le premier token s'il existe
-      await dbHelper.deleteFirstToken();
-      await dbHelper.insertToken(token);
-    }
-    var tokenBdd = await dbHelper.getFirstToken();
-    print('Token: $token');
-    print('Token base de données avec get : ' + tokenBdd!);
+
+    await dbHelper.deleteDatabase();
+    await dbHelper.fillDatabaseWithToken(token);
   }
 }
 
