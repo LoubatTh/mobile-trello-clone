b494761c488538d3a913ea350c370949fdf36f1b
LoubatTh <thomas.loubat@epitech.eu>
2024-03-16 16:17:05 +0100

    pages

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:app/pages/board_page.dart';
+import 'package:app/pages/workspace_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Column(
+        children: <Widget>[
+          const Text('Welcome to the Home Page'),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const BoardPage()),
+              );
+            },
+            child: const Text('Go to Workspace Page'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const BoardPage()),
+              );
+            },
+            child: const Text('Go to Workspace Page'),
+          ),
+        ],
+      ),
+    );
+  }
+}
