27b178bdf45ed14df447743ad144b6c976f2405c
Ariirau FUCKS <ariirau.fucks@epitech.eu>
2024-03-17 12:36:43 +0100

    fix: resolve and debug conflicts

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,14 +1,40 @@
-import 'package:app/services/user_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:app/services/user_helper.dart';
+import 'package:app/pages/home_page.dart';
 
-final Uri _url = Uri.parse(
-    'https://trello.com/1/authorize?expiration=never&scope=read,write,account&response_type=token&key=fddd47f32d57f1779100f625dc67f2b6');
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TrellTech',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.grey,
+          brightness: Brightness.dark,
+          backgroundColor: Colors.grey[900],
+        ),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
 
 class MyHomePage extends StatelessWidget {
   final TextEditingController _textController = TextEditingController();
 
-  MyHomePage({super.key});
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +60,13 @@ class MyHomePage extends StatelessWidget {
                   ),
                 ),
                 ElevatedButton(
-                  onPressed: () => _sendToken(_textController.text),
+                  onPressed: () async {
+                    await _sendToken(_textController.text);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
                   child: const Text('Send Token'),
                 ),
               ],
@@ -45,15 +77,22 @@ class MyHomePage extends StatelessWidget {
     );
   }
 
-  void _sendToken(String token) async {
+  Future<void> _sendToken(String token) async {
     final dbHelper = DatabaseHelper.instance;
 
     await dbHelper.deleteDatabase();
     await dbHelper.fillDatabaseWithToken(token);
+    print('print database : ');
+    await dbHelper.printDatabaseColumns();
+    await dbHelper.printDatabase();
   }
 }
 
 Future<void> _launchInWebView() async {
+  final apiurl = dotenv.env['API_URL'];
+  final apikey = dotenv.env['API_KEY'];
+  final _url = Uri.parse(
+      '$apiurl/authorize?expiration=never&scope=read,write,account&response_type=token&key=$apikey');
   if (!await launchUrl(_url, mode: LaunchMode.inAppBrowserView)) {
     throw Exception('Could not launch $_url');
   }
--- lib/services/user_helper.dart
+++ lib/services/user_helper.dart
@@ -1,5 +1,5 @@
 import 'package:dio/dio.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart'; // Importez sqflite_ffi
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:path/path.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -7,82 +7,112 @@ class DatabaseHelper {
   static Database? _database;
   final client = Dio();
   final apikey = dotenv.env['API_KEY'];
+  final apiurl = dotenv.env['API_URL'];
 
   DatabaseHelper._privateConstructor();
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
+  /// Initialise et retourne la base de données.
   Future<Database> get database async {
     if (_database != null) return _database!;
 
     sqfliteFfiInit();
-    String path = join(await getDatabasesPath(), 'member_db.db');
+    String path = join(await getDatabasesPath(), 'user_db.db');
+
     _database = await openDatabase(path, version: 1,
         onCreate: (Database db, int version) async {
       await db.execute(
-          'CREATE TABLE member(id INTEGER PRIMARY KEY, userID TEXT, memberID TEXT)');
+          'CREATE TABLE user(id INTEGER PRIMARY KEY, userID TEXT, memberID TEXT, token TEXT, username TEXT)');
     });
     return _database!;
   }
 
-  // Méthode pour récupérer l'ID de l'utilisateur et son memberID
-  fillDatabaseWithToken(String token) async {
+  /// Remplit la base de données avec les informations du token.
+  Future<void> fillDatabaseWithToken(String token) async {
     final db = await database;
-    final url =
-        'https://api.trello.com/1/tokens/$token?key=$apikey&token=$token';
+    final url = '$apiurl/tokens/$token?key=$apikey&token=$token';
+    final url_member =
+        '$apiurl/members/me?key=$apikey&token=$token&fields=username';
 
     try {
       final response = await client.get(url);
+      final response_member = await client.get(url_member);
 
-      if (response.statusCode == 200) {
-        await db.insert('member', {
+      if (response.statusCode == 200 && response_member.statusCode == 200) {
+        await db.insert('user', {
           'userID': response.data['id'],
-          'memberID': response.data['idMember']
+          'memberID': response.data['idMember'],
+          'token': token,
+          'username': response_member.data['username'],
         });
-        print("Informations added to database");
+        print("Informations ajoutées à la base de données");
+        await printDatabase();
       } else {
         print('${response.statusCode} : ${response.data.toString()}');
+        print(
+            '${response_member.statusCode} : ${response_member.data.toString()}');
       }
     } catch (error) {
-      print(error);
+      print("Erreur lors de la récupération des données: $error");
     }
   }
 
-  // Méthode pour récupérer le memberID de l'utilisateur
+  /// Récupère l'ID de l'utilisateur.
   Future<String?> getUserID() async {
     Database db = await database;
-    List<Map<String, dynamic>> member = await db.query('member', limit: 1);
+    List<Map<String, dynamic>> member = await db.query('user', limit: 1);
     if (member.isEmpty) {
       return null;
     }
     return member.first['userID'];
   }
 
-  // Méthode pour récupérer le memberID de l'utilisateur
+  /// Récupère le memberID de l'utilisateur.
   Future<String?> getMemberID() async {
     Database db = await database;
-    List<Map<String, dynamic>> member = await db.query('member', limit: 1);
+    List<Map<String, dynamic>> member = await db.query('user', limit: 1);
     if (member.isEmpty) {
       return null;
     }
     return member.first['memberID'];
   }
 
-  // Méthode pour supprimer le premier token de la base de données
-  Future<int> deleteFirstToken() async {
+  /// Récupère le token de l'utilisateur.
+  Future<String?> getToken() async {
     Database db = await database;
-    return await db.delete('member');
+    List<Map<String, dynamic>> member = await db.query('user', limit: 1);
+    if (member.isEmpty) {
+      print("Aucun token trouvé");
+      return null;
+    }
+    return member.first['token'];
   }
 
-  // Méthode pour supprimer le membre de la base de données
+  /// Supprime le membre de la base de données.
   Future<int> deleteDatabase() async {
     Database db = await database;
-    return await db.delete('member');
+    return await db.delete('user');
   }
 
-  // Méthode pour print la base de données
+  /// Affiche les données de la base de données.
   Future<void> printDatabase() async {
     Database db = await database;
-    List<Map<String, dynamic>> member = await db.query('member');
+    List<Map<String, dynamic>> member = await db.query('user');
     print(member);
+  }
+
+  Future<void> printDatabaseColumns() async {
+    Database database = await openDatabase(
+      join(await getDatabasesPath(), 'user_db.db'),
+    );
+
+    List<Map<String, dynamic>> columns = await database.rawQuery(
+      "PRAGMA table_info('user')",
+    );
+
+    print('Columns of the member table:');
+    for (var column in columns) {
+      print(column['name']);
+    }
   }
 }
