b1c672a2ed5aa24f53bde0bb7359321851e1b167
AriirauF <ariirau.fucks@epitech.eu>
2024-03-30 21:40:28 +0100

    refactor: update code

--- lib/widgets/card_list_widget.dart
+++ lib/widgets/card_list_widget.dart
@@ -4,19 +4,17 @@ import 'package:flutter/material.dart';
 class CardListWidget extends StatefulWidget {
   final String listId, listName;
 
-  const CardListWidget(
-      {super.key, required this.listId, required this.listName});
+  const CardListWidget({
+    required Key key,
+    required this.listId,
+    required this.listName,
+  }) : super(key: key);
 
   @override
   State<CardListWidget> createState() => _CardListWidgetState();
 }
 
 class _CardListWidgetState extends State<CardListWidget> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -26,11 +24,15 @@ class _CardListWidgetState extends State<CardListWidget> {
           widget.listName,
           style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
         ),
+        const SizedBox(height: 4.0),
         Expanded(
           child: SingleChildScrollView(
             child: Column(
               children: [
-                CardList(key: const Key("cardList"), listId: widget.listId),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.75, 
+                  child: CardList(key: const Key("cardList"), listId: widget.listId),
+                ),
                 const CreateCardButton(
                   key: Key('createCardButton'),
                 ),
@@ -45,39 +47,38 @@ class _CardListWidgetState extends State<CardListWidget> {
 
 class CreateCardButton extends StatelessWidget {
   const CreateCardButton({
-    super.key,
-  });
+    required Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () => {
-        showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: const Text('Create a new card'),
-                content: const TextField(
-                  decoration: InputDecoration(hintText: 'Enter the card name'),
-                ),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('Cancel'),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      // createCard(textController.text);
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('Create'),
-                  ),
-                ],
-              );
-            })
-      },
+      onPressed: () => showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Create a new card'),
+            content: const TextField(
+              decoration: InputDecoration(hintText: 'Enter the card name'),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  // createCard(textController.text);
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Create'),
+              ),
+            ],
+          );
+        },
+      ),
       child: const Text('Add a card'),
     );
   }
